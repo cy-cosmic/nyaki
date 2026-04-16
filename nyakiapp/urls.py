f2246f6c862a9bababb92cc86ae86eda68a7a946
Cyprian Munene <munenecyp@gmail.com>
2026-04-16 10:54:19 -0700

    add about, services, gallery, and contact pages

--- nyakiapp/urls.py
+++ nyakiapp/urls.py
@@ -4,4 +4,8 @@ from nyakiapp import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about-us", views.about, name="about"),
+    path("services", views.services, name="services"),
+    path("gallery", views.gallery, name="gallery"),
+    path("contact-us", views.contact, name="contact"),
 ]
--- nyakiapp/views.py
+++ nyakiapp/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html", locals())
+
+
+def about(request):
+    return render(request, "about.html", locals())
+
+
+def services(request):
+    return render(request, "services.html", locals())
+
+def contact(request):
+    return render(request, "contact.html", locals())
+
+
+def gallery(request):
+    return render(request, "gallery.html", locals())
