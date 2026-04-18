804addac2d96f014bafbc6698ab98efedb758094
Cyprian Munene <munenecyp@gmail.com>
2026-04-18 14:37:53 -0700

    protect admin view with username and pw

--- adminapp/views.py
+++ adminapp/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, redirect
 from adminapp.models import ForestRidgeHome, RStreetHome, FoodAndActivities
 
 
-@login_required
+@login_required(login_url="/login/")
 # Create your views here.
 def index(request):
     return render(request, "admin/index.html", locals())
@@ -27,7 +27,7 @@ def login_view(request):
     return render(request, "admin/login.html", locals())
 
 
-@login_required
+@login_required(login_url="/login/")
 def forest_ridge(request):
     if request.method == "POST":
         image = request.FILES.get("image")
@@ -37,7 +37,7 @@ def forest_ridge(request):
     return render(request, "admin/forest-ridge.html", locals())
 
 
-@login_required
+@login_required(login_url="/login/")
 def rstreet(request):
     if request.method == "POST":
         image = request.FILES.get("image")
@@ -47,7 +47,7 @@ def rstreet(request):
     return render(request, "admin/rstreet.html", locals())
 
 
-@login_required
+@login_required(login_url="/login/")
 def activities(request):
     if request.method == "POST":
         image = request.FILES.get("image")
@@ -57,7 +57,7 @@ def activities(request):
     return render(request, "admin/activities.html", locals())
 
 
-@login_required
+@login_required(login_url="/login/")
 def delete_forest(request, id):
     if request.method == "POST":
         obj = ForestRidgeHome.objects.get(id=id)
@@ -65,7 +65,7 @@ def delete_forest(request, id):
         return JsonResponse({"success": True})
 
 
-@login_required
+@login_required(login_url="/login/")
 def delete_rstreet(request, id):
     if request.method == "POST":
         obj = RStreetHome.objects.get(id=id)
@@ -73,7 +73,7 @@ def delete_rstreet(request, id):
         return JsonResponse({"success": True})
 
 
-@login_required
+@login_required(login_url="/login/")
 def delete_activities(request, id):
     if request.method == "POST":
         obj = FoodAndActivities.objects.get(id=id)
